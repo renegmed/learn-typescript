d569b7255f555ef98a5ff0c753047830fbaa3990
Rene Medalla <renemed@pop-os.localdomain>
2022-08-24 16:27:07 -0400

    sec 24: 351.When inference doesn't work

--- features/2.annotations/variables.ts
+++ features/2.annotations/variables.ts
@@ -43,6 +43,33 @@ console.log(coordinates);  // { x: 10, y: 20 }
  
 //coordinates.xdsdfsdd
 
+// 2. When we declare a variable on lne line and 
+//   initialize it later 
 
+let words = ['red', 'green', 'blue'];
+let foundWord = false;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+words.map((word) => {
+    if (word == 'green') {
+        console.log("word", word, "found.");
+    }
+});
+
+
+// 3. Variable whose type cannot be inferred correctly 
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
  
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
+
 
